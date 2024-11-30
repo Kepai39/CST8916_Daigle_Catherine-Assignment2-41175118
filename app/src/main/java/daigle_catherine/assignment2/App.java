ce02c53176d60733c2d0a2e444f834728da8ba93
ehre0004 <ehre0004@algonquinlive.com>
2024-11-30 12:11:37 -0500

    made more progress on iot device sim

--- app/src/main/java/daigle_catherine/assignment2/App.java
+++ app/src/main/java/daigle_catherine/assignment2/App.java
@@ -12,15 +12,94 @@ import com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
+    private static final String[] LOCATIONS = {"Dow's Lake", "Fifth Avenue", "NAC"};
+
+    /**
+     * Connection string for IoT Hub
+     */
+    private static final String IOTHUB_CONN_STR = System.getenv("IOTHUB_CONN_STR");
+
+    /**
+     * How many requests for this IoT simulator to send
+     */
+    private static final String NUM_REQUESTS_SEND = System.getenv("NUM_REQUESTS_SEND");
+
     public static void main(String[] args) {
         // TODO set up connection
-        // TODO set up messages
-        // TODO send messages
+        IotHubClientProtocol protocol = IotHubClientProtocol.MQTT;
+        if (IOTHUB_CONN_STR.isBlank() || IOTHUB_CONN_STR == null) {
+            System.out.println("IoT Hub Connection String is blank or null");
+            return;
+        }
+        int numRequests = 0;
+        try {
+            numRequests = Integer.parseInt(NUM_REQUESTS_SEND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Environment variables OK.");
+
+        ModuleClient client = new ModuleClient(IOTHUB_CONN_STR, protocol);
+        client.setMessageCallback(new MessageCallback(), null);
+        client.setConnectionStatusChangeCallback(new IotHubConnectionStatusChangeCallbackLogger(), new Object());
+        try {
+            client.open(true);
+        } catch (IotHubClientException e) {
+            System.out.println("Could not open connection.");
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("Sending messages...");
+        for (int i = 0; i < numRequests; i++) {
+            String msg1 = randomizeIoTData(LOCATIONS[0]); // dows lake
+            String msg2 = randomizeIoTData(LOCATIONS[1]); // fifth ave
+            String msg3 = randomizeIoTData(LOCATIONS[2]); // nac
+
+            // prepare message
+            for (int y = 0; y < 3; y++) {
+                /*
+                Message msg = new Message(msg1);
+                msg.setContentType("application/json");
+                msg.setMessageId(java.util.UUID.randomUUID().toString());
+                msg.setExpiryTime(D2C_MESSAGE_TIMEOUT_MILLISECONDS);
+                System.out.println(msg1);
+                client.sendEvent(msg, D2C_MESSAGE_TIMEOUT_MILLISECONDS);
+                System.out.println("Successfully sent the message");
+                */
+            }
+            
+        }
+    }
+
+    /**
+     * Randomizes data and returns JSON string with randomized IoT Data. Temperatures are within bounds of -30 to 0 (surfaceTemperature) or -20 to 10 (externalTemperature).
+     * @return String of JSON with randomized IoT Data
+     */
+    private static String randomizeIoTData(String location) {
+        Random random = new Random();
+        int iceThickness = random.ints(10, 41).findFirst().getAsInt();
+        double surfaceTemp = Math.random() * (0 + 30) - 30;
+        int snowAccumulation = random.ints(0, 10).findFirst().getAsInt();
+        double externalTemp = Math.random() * (10 + 20) - 20;
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'"); 
+        String timestamp = sdf.format(date);
+        return "{\"location\": \""+location+"\", "
+            + "\"iceThickness\": "+iceThickness+", "
+            + "\"surfaceTemperature\": "+surfaceTemp+", "
+            + "\"snowAccumulation\": "+snowAccumulation+", "
+            + "\"externalTemperature\": "+externalTemp+", "
+            + "\"timestamp\": "+timestamp+"}";
     }
 
     /**
@@ -54,5 +133,18 @@ public class App {
 
         }
         
+    }
+
+    /**
+     * From {@link https://github.com/Azure/azure-iot-sdk-java/blob/main/iothub/device/iot-device-samples/send-receive-module-sample/src/main/java/samples/com/microsoft/azure/sdk/iot/SendReceiveModuleSample.java}
+     */
+    protected static class MessageCallback implements com.microsoft.azure.sdk.iot.device.MessageCallback
+    {
+        public IotHubMessageResult onCloudToDeviceMessageReceived(Message msg, Object context)
+        {
+            System.out.println(
+                "Received message with content: " + new String(msg.getBytes(), Message.DEFAULT_IOTHUB_MESSAGE_CHARSET));
+            return IotHubMessageResult.COMPLETE;
+        }
     }
 }
