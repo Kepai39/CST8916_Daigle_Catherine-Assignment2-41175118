a20956a70d0307b905ecab04022e2302977d0578
ehre0004 <ehre0004@algonquinlive.com>
2024-11-30 12:27:24 -0500

    soft finish of app, needs testing

--- app/src/main/java/daigle_catherine/assignment2/App.java
+++ app/src/main/java/daigle_catherine/assignment2/App.java
@@ -9,19 +9,13 @@ import com.microsoft.azure.sdk.iot.device.exceptions.IotHubClientException;
 import com.microsoft.azure.sdk.iot.device.transport.IotHubConnectionStatus;
 /* */
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 
 public class App {
     private static final String[] LOCATIONS = {"Dow's Lake", "Fifth Avenue", "NAC"};
+    private static final int D2C_MESSAGE_TIMEOUT_MILLISECONDS = 10000;
 
     /**
      * Connection string for IoT Hub
@@ -34,7 +28,6 @@ public class App {
     private static final String NUM_REQUESTS_SEND = System.getenv("NUM_REQUESTS_SEND");
 
     public static void main(String[] args) {
-        // TODO set up connection
         IotHubClientProtocol protocol = IotHubClientProtocol.MQTT;
         if (IOTHUB_CONN_STR.isBlank() || IOTHUB_CONN_STR == null) {
             System.out.println("IoT Hub Connection String is blank or null");
@@ -61,21 +54,22 @@ public class App {
         }
         System.out.println("Sending messages...");
         for (int i = 0; i < numRequests; i++) {
-            String msg1 = randomizeIoTData(LOCATIONS[0]); // dows lake
-            String msg2 = randomizeIoTData(LOCATIONS[1]); // fifth ave
-            String msg3 = randomizeIoTData(LOCATIONS[2]); // nac
+            String[] msgs = {randomizeIoTData(LOCATIONS[0]), randomizeIoTData(LOCATIONS[1]), randomizeIoTData(LOCATIONS[2])};
 
             // prepare message
             for (int y = 0; y < 3; y++) {
-                /*
-                Message msg = new Message(msg1);
+                Message msg = new Message(msgs[y]);
                 msg.setContentType("application/json");
                 msg.setMessageId(java.util.UUID.randomUUID().toString());
                 msg.setExpiryTime(D2C_MESSAGE_TIMEOUT_MILLISECONDS);
-                System.out.println(msg1);
-                client.sendEvent(msg, D2C_MESSAGE_TIMEOUT_MILLISECONDS);
-                System.out.println("Successfully sent the message");
-                */
+                System.out.println(msgs[y]);
+                try {
+                    client.sendEvent(msg, D2C_MESSAGE_TIMEOUT_MILLISECONDS);
+                } catch (IllegalStateException | InterruptedException | IotHubClientException e) {
+                    e.printStackTrace();
+                    return;
+                }
+                System.out.println("Message sent.");
             }
             
         }
